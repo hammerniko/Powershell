38a01c2477b64d49ef647edf607a80d462a9bff9
hammerniko <hammer.nikolai@googlemail.com>
2021-12-09 12:28:23 +0100

    Dateien drucken getestet

--- DateienDrucken.ps1
+++ DateienDrucken.ps1
@@ -1,10 +1,16 @@
 # Speicherort
-$path = "D:\FilesToPrint\"
-echo "Drucke alle Dateien in $path"
+cls
+$ordner = "D:\FilesToPrint\"
+echo "Drucke alle Dateien im Ordner:"$ordner
 
-# Alle Files drucken
-$files = Get-ChildItem -Path $path -recurse -Filter "*.java"
-foreach ($file in $files) { Start-Process –FilePath $path\$file –Verb Print -PassThru | %{sleep 10;$_} | kill     }
+#Alle Files auflisten
+$files = Get-ChildItem -Path $ordner -recurse -include "*.java"
 
+
+
+#Alle Files drucken
+foreach ($file in $files) { 
+  Get-Content $file | Out-String | Out-Printer 
+}
 
 echo "Druckauftrag beendet"
